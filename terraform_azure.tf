336f9b795bd60a5829308987917ce84beb8b9507
McAtkin <35996616+McAtkin@users.noreply.github.com>
2018-09-27 15:18:04 +0100

    Update terraform_azure.tf

--- terraform_azure.tf
+++ terraform_azure.tf
@@ -6,8 +6,8 @@ variable "resourcename" {
 provider "azurerm" {
     subscription_id = "${var.ARM_SUBSCRIPTION_ID}"
     client_id       = "${var.ARM_CLIENT_ID}"
-    client_secret   = "${var.ARM_SUBSCRIPTION_ID}"
-    tenant_id       = "${var.ARM_CLIENT_SECRET}"
+    client_secret   = "${var.ARM_CLIENT_SECRET}"
+    tenant_id       = "${var.ARM_TENANT_ID}"
 }
 
 # Create a resource group if it doesn’t exist
