0cc227f5bf6d1cb67702b2d6c41154c33b3f20f8
McAtkin <35996616+McAtkin@users.noreply.github.com>
2018-09-27 15:25:13 +0100

    Update terraform_azure.tf

--- terraform_azure.tf
+++ terraform_azure.tf
@@ -2,6 +2,11 @@ variable "resourcename" {
   default = "myResourceGroup"
 }
 
+variable "ARM_SUBSCRIPTION_ID" {}
+variable "ARM_CLIENT_ID" {}
+variable "ARM_CLIENT_SECRET" {}
+variable "ARM_TENANT_ID" {}
+
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
     subscription_id = "${var.ARM_SUBSCRIPTION_ID}"
